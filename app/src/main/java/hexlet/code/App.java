8c84273ce8554a2ce6ac7d2c49e0f11927aadb94
Sanapol <junrey@list.ru>
2024-10-17 18:00:40 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Utilit", mixinStandardHelpOptions = true)
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(
+            names = { "-f", "--format" },
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
