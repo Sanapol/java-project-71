42b00ed4df07145d2093a9d1f9651d151f0f787f
Sanapol <junrey@list.ru>
2024-10-20 10:44:06 +0300

    update Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,10 +1,16 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 import java.util.Map;
 
 public class Parser {
 
-    public static Map<String, Object> parse(String content, String format) {
-
+    public static Map<String, Object> parse(String content) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> scoreByName = mapper.readValue(content, Map.class);
+        return scoreByName;
     }
 }
