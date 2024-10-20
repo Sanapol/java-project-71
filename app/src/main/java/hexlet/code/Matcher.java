ca064531626d834fe2e638483edab19b303eaae2
Sanapol <junrey@list.ru>
2024-10-20 05:22:21 +0300

    add Matcher

---
+++ app/src/main/java/hexlet/code/Matcher.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.List;
+import java.util.Map;
+
+public class Matcher {
+
+    public static List<Map<String, Object>> match(Map<String, Object> parsedFile1, Map<String, Object> parsedFile2) {
+
+    }
+}
