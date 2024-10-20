377321122a771d7b3395952128d9c9e9f7ba5d82
Sanapol <junrey@list.ru>
2024-10-20 05:22:46 +0300

    add Reader

---
+++ app/src/main/java/hexlet/code/Reader.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+public class Reader {
+
+    public static String readFile(String filepath) {
+        return "";
+    }
+
+    public static String getFormat(String filepath) {
+        return "";
+    }
+}
