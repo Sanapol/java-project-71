181af641a34d69b9dca72f0853a35c5ed624d84d
Sanapol <junrey@list.ru>
2024-10-23 18:01:28 +0300

    update Reader

--- app/src/main/java/hexlet/code/Reader.java
+++ app/src/main/java/hexlet/code/Reader.java
@@ -1,13 +1,15 @@
 package hexlet.code;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Reader {
 
-    public static String readFile(String filepath) throws Exception{
-        var path = Paths.get(filepath);
-        return Files.readString(path).trim();
+    public static String readFile(String fileName) throws Exception {
+        Path filepath = Paths.get("src", "main", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+        return Files.readString(filepath).trim();
     }
 
     public static String getFormat(String filepath) {
