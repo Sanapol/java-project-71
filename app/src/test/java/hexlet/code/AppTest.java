b5400e54d34060c5c35b588c7cb29752aa685f42
Sanapol <junrey@list.ru>
2024-10-23 17:56:36 +0300

    update AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,19 +2,21 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
 
     @Test
     public void app() throws Exception {
-        var file1 = Differ.generate("src/test/resources/file1.json",
-                "src/test/resources/file2.json");
-        var file2 = Differ.generate("src/test/resources/file2.json",
-                "src/test/resources/file2.json");
+        var result = Differ.generate("file1.json", "file2.json");
+        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answer")).trim();
 
-        var result = Matcher.match(file1, file2);
         System.out.println(result);
-        assertEquals(file1, file1);
+        System.out.println("=======================");
+        System.out.println(answer);
+        assertEquals(result, answer);
     }
 }
