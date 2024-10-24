e64a455db8366eff60b601b7d33d5b7ba4e0605f
Sanapol <junrey@list.ru>
2024-10-24 13:28:15 +0300

    update AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -10,8 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AppTest {
 
     @Test
-    public void app() throws Exception {
-        var result = Differ.generate("file1.json", "file2.json");
+    public void test1() throws Exception {
+        var result = Differ.generate("file1", "file2", "json");
+        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answer")).trim();
+
+        System.out.println(result);
+        System.out.println("=======================");
+        System.out.println(answer);
+        assertEquals(result, answer);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        var result = Differ.generate("src/main/resources/fixtures/file1.json", "src/main/resources/fixtures/file2.json");
         var answer = Files.readString(Paths.get("src/test/resources/fixtures/answer")).trim();
 
         System.out.println(result);
