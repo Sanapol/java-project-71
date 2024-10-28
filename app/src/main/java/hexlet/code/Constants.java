0e5645edf0e1273d462bdfcff1ae3f4cc5756930
Sanapol <junrey@list.ru>
2024-10-28 19:13:21 +0300

    add Constants.java.java

---
+++ app/src/main/java/hexlet/code/Constants.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public class Constants {
+
+    public static final int UPDATE_MAP = 0;
+    public static final int ADD_MAP = 1;
+    public static final int REMOVE_MAP = 2;
+    public static final int NOTHING_MAP = 3;
+}
