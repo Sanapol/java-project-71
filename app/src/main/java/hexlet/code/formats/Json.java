8b14b8c26d42166ce888341df0c7f7190e764bcb
Sanapol <junrey@list.ru>
2024-10-28 19:38:08 +0300

    update AppTest.java, answerPlain, Json.java

--- app/src/main/java/hexlet/code/formats/Json.java
+++ app/src/main/java/hexlet/code/formats/Json.java
@@ -23,7 +23,7 @@ public class Json {
             objectMapper.writeValueAsString(list.append("\n  ").append(readyKeys).append(": ")
                     .append(maps.get(readyKeys)));
         }
-        return list.toString();
+        return list.toString().trim();
     }
 
     private static List<String> mergeKeys(List<Map<String, Object>> listMaps) {
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -12,7 +12,7 @@ class AppTest {
     @Test
     public void test1() throws Exception {
         var result = Differ.generate("file1.yaml", "file2.yaml");
-        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerStylish"));
+        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerStylish")).trim();
 
         assertEquals(result, answer);
     }
@@ -21,7 +21,7 @@ class AppTest {
     public void test2() throws Exception {
         var result = Differ.generate("src/main/resources/fixtures/file1.json",
                 "src/main/resources/fixtures/file2.json", "plain");
-        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerPlain"));
+        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerPlain")).trim();
 
         assertEquals(result, answer);
     }
@@ -30,7 +30,7 @@ class AppTest {
     public void test3() throws Exception {
         var result = Differ.generate("src/main/resources/fixtures/file1.json",
                 "src/main/resources/fixtures/file2.json", "json");
-        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerJson"));
+        var answer = Files.readString(Paths.get("src/test/resources/fixtures/answerJson")).trim();
 
         assertEquals(result, answer);
     }
